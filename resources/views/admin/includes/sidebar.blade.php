6acae6376b414af3211aa4301907383ae0e6be52
excellis-it <web.excellisit@gmail.com>
2025-05-05 12:46:53 +0530

    first push

---
+++ resources/views/admin/includes/sidebar.blade.php
@@ -0,0 +1,48 @@
+
+
+
+<div class="main-sidebar sidebar-style-2" tabindex="1">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="{{ route('admin.dashboard') }}"><span class="logo-name"><img
+                        src="{{ asset('admin_assets/img/logo.png') }}" /></span> </a>
+            <a href="{{ route('admin.dashboard') }}"><span class="logo-fm"><img
+                        src="{{ asset('admin_assets/img/logo.png') }}" /></span> </a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Main</li>
+            <li class="dropdown {{ Request::is('admin/dashboard*') ? 'active' : '' }}">
+                <a href="{{ route('admin.dashboard') }}">
+                    <i class="fas fa-home"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+
+            <li class="dropdown">
+                <a href="javascript:void(0);"
+                    class="menu-toggle nav-link has-dropdown {{ Request::is('admin/profile*') || Request::is('admin/password*') || Request::is('admin/detail*') ? 'active' : '' }}">
+                    <i class="fas fa-user-circle"></i>
+                    <span>Manage Account</span>
+                </a>
+                <ul class="dropdown-menu"
+                    style="{{ Request::is('admin/profile*') || Request::is('admin/password*') || Request::is('admin/detail*') ? 'display: block;' : 'display: none;' }}">
+                    <li class="{{ Request::is('admin/profile*') ? 'active' : '' }}"><a class="nav-link"
+                            href="{{ route('admin.profile') }}"><i class="fas fa-user"></i> My Profile</a></li>
+                    <li class="{{ Request::is('admin/password*') ? 'active' : '' }}"><a class="nav-link"
+                            href="{{ route('admin.password') }}"><i class="fas fa-lock"></i> Change Password</a></li>
+                </ul>
+            </li>
+
+            <li class="dropdown">
+                <a href="javascript:void(0);" class="menu-toggle nav-link has-dropdown {{ Request::is('admin/customers*') ? 'active' : ' ' }}">
+                    <i class="ph ph-user-list"></i>
+                    <span> User Management</span>
+                </a>
+                <ul class="dropdown-menu" style="{{ Request::is('admin/customers*') ? 'display: block;' : 'display: none;' }}">
+                    <li class="{{ Request::is('admin/customers/create') ? 'active' : ' ' }}"><a class="nav-link" href="{{ route('customers.create') }}">Create  User</a></li>
+                    <li class="{{ Request::is('admin/customers') ? 'active' : ' ' }}"><a class="nav-link" href="{{ route('customers.index') }}"> User List</a></li>
+                </ul>
+            </li>
+        </ul>
+    </aside>
+</div>
---
+++ routes/web.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AuthController;
+use App\Http\Controllers\Admin\ForgetPasswordController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\ProfileController;
+use App\Http\Controllers\Admin\CustomerController;
+use App\Http\Controllers\Admin\SellerController;
+use App\Http\Controllers\Frontend\HomeController;
+use Illuminate\Support\Facades\Artisan;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Clear cache
+Route::get('clear', function () {
+    Artisan::call('optimize:clear');
+    return "Optimize clear has been successfully";
+});
+
+Route::get('/admin', [AuthController::class, 'redirectAdminLogin']);
+Route::get('/admin/login', [AuthController::class, 'login'])->name('admin.login');
+Route::post('/login-check', [AuthController::class, 'loginCheck'])->name('admin.login.check');  //login check
+Route::post('forget-password', [ForgetPasswordController::class, 'forgetPassword'])->name('admin.forget.password');
+Route::post('change-password', [ForgetPasswordController::class, 'changePassword'])->name('admin.change.password');
+Route::get('forget-password/show', [ForgetPasswordController::class, 'forgetPasswordShow'])->name('admin.forget.password.show');
+Route::get('reset-password/{id}/{token}', [ForgetPasswordController::class, 'resetPassword'])->name('admin.reset.password');
+Route::post('change-password', [ForgetPasswordController::class, 'changePassword'])->name('admin.change.password');
+
+Route::group(['middleware' => ['admin'], 'prefix'=>'admin'], function () {
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+    Route::get('profile', [ProfileController::class, 'index'])->name('admin.profile');
+    Route::post('profile/update', [ProfileController::class, 'profileUpdate'])->name('admin.profile.update');
+    Route::get('logout', [AuthController::class, 'logout'])->name('admin.logout');
+
+    Route::prefix('password')->group(function () {
+        Route::get('/', [ProfileController::class, 'password'])->name('admin.password'); // password change
+        Route::post('/update', [ProfileController::class, 'passwordUpdate'])->name('admin.password.update'); // password update
+    });
+
+    Route::resources([
+        'customers' => CustomerController::class,
+    ]);
+    //  Customer Routes
+    Route::prefix('customers')->group(function () {
+        Route::get('/customer-delete/{id}', [CustomerController::class, 'delete'])->name('customers.delete');
+    });
+    Route::get('/changeCustomerStatus', [CustomerController::class, 'changeCustomersStatus'])->name('customers.change-status');
+    Route::get('/customer-fetch-data', [CustomerController::class, 'fetchData'])->name('customers.fetch-data');
+
+});
+
+
+
+Route::get('/', [HomeController::class, 'home'])->name('home');
